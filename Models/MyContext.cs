8fbad5363f78da6c830718a306577897a9efbfdc
KevinKatiraj <katirajk@gmail.com>
2022-10-03 16:11:33 +0200

    Login Done

--- Models/MyContext.cs
+++ Models/MyContext.cs
@@ -4,11 +4,12 @@ Disabled Warning: "Non-nullable field must contain a non-null value when exiting
 We can disable this safely because we know the framework will assign non-null values when it constructs this class for us.
 */
 using Microsoft.EntityFrameworkCore;
-namespace Monsters.Models;
+namespace PizzaTime.Models;
 // the MyContext class representing a session with our MySQL database, allowing us to query for or save data
 public class MyContext : DbContext 
 { 
     public MyContext(DbContextOptions options) : base(options) { }
     // the "Monsters" table name will come from the DbSet property name
     // public DbSet<Monster> Monsters { get; set; } 
+    public DbSet<User> Users { get; set; }
 }
